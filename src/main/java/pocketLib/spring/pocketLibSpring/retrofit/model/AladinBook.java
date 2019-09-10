dec146a6feee98d551d9b0d468425335ac4963fc
dominic <guswhd1993@gmail.com>
2019-09-10 14:41:28 +0900

    .

--- src/main/java/pocketLib/spring/pocketLibSpring/retrofit/model/AladinBook.java
+++ src/main/java/pocketLib/spring/pocketLibSpring/retrofit/model/AladinBook.java
@@ -1,62 +1,4 @@
-<<<<<<< HEAD
-package pocketLib.spring.pocketLibSpring.retrofit.model;
 
-import java.util.List;
-
-import com.google.gson.annotations.SerializedName;
-
-import lombok.Data;
-
-@Data
-public class AladinBook {
-
-	/*
-	 * @SerializedName("title") public String title;
-	 * 
-	 * @SerializedName("searchCategoryName") public String searchCategoryName;
-	 */
-
-	@SerializedName("item")
-	public List<Item> item;
-
-	@Data
-	public class Item {
-		@SerializedName("link")
-		public String link;
-		@SerializedName("cover")
-		public String cover;
-		@SerializedName("title")
-		public String title;
-		@SerializedName("author")
-		public String author;
-		@SerializedName("pubDate")
-		public String pubDate;
-		@SerializedName("description")
-		public String description;
-		@SerializedName("isbn")
-		public String isbn;
-		
-		@SerializedName("publisher")
-		public String publisher;
-		
-		@SerializedName("customerReviewRank")
-		public int customerReviewRank;
-
-		@SerializedName("categoryName")
-		public String categoryName;
-
-		@SerializedName("categoryId")
-		public int categoryId;
-		
-		@SerializedName("priceSales")
-		public int priceSales;
-
-		@SerializedName("priceStandard")
-		public int priceStandard;
-	}
-
-}
-=======
 package pocketLib.spring.pocketLibSpring.retrofit.model;
 
 import java.util.List;
@@ -113,4 +55,4 @@ public class AladinBook {
 	}
 
 }
->>>>>>> f1cd26bbb41b54115ce93ea2b1be85db5b4cc012
+
--- src/main/java/pocketLib/spring/pocketLibSpring/retrofit/model/AladinBookList.java
+++ src/main/java/pocketLib/spring/pocketLibSpring/retrofit/model/AladinBookList.java
@@ -1,75 +1,5 @@
-<<<<<<< HEAD
-package pocketLib.spring.pocketLibSpring.retrofit.model;
-
-import java.util.List;
 
 
-import com.google.gson.annotations.SerializedName;
-
-import lombok.Data;
-
-@Data
-public class AladinBookList {
-	
-	@SerializedName("title")
-	public String title;
-
-	@SerializedName("searchCategoryId")
-	public int searchCategoryId;
-	
-	@SerializedName("searchCategoryName")
-	public String searchCategoryName;
-	
-	@SerializedName("item")
-	public List<Item> item;
-
-	
-	@Data
-	public class Item {
-		@SerializedName("link")
-		public String link;
-
-		@SerializedName("cover")
-		public String cover;
-		@SerializedName("title")
-		public String title;
-		@SerializedName("author")
-		public String author;
-		@SerializedName("pubDate")
-		public String pubDate;
-		@SerializedName("description")
-		public String description;
-		
-		@SerializedName("isbn")
-		public String isbn;
-		
-		
-		@SerializedName("publisher")
-		public String publisher;
-		
-		@SerializedName("customerReviewRank")
-		public int customerReviewRank;
-
-		@SerializedName("categoryName")
-		public String categoryName;
-		
-		
-		@SerializedName("categoryId")
-		public int categoryId;
-		
-		@SerializedName("priceSales")
-		public int priceSales;
-
-		@SerializedName("priceStandard")
-		public int priceStandard;
-	
-		@SerializedName("bestRank")
-		public int bestRank;
-	
-	
-	}
-}
-=======
 package pocketLib.spring.pocketLibSpring.retrofit.model;
 
 import java.util.List;
@@ -113,9 +43,9 @@ public class AladinBookList {
 		@SerializedName("isbn")
 		public String isbn;
 		
-		
 		@SerializedName("publisher")
 		public String publisher;
+		
 		@SerializedName("customerReviewRank")
 		public int customerReviewRank;
 
@@ -138,4 +68,4 @@ public class AladinBookList {
 	
 	}
 }
->>>>>>> f1cd26bbb41b54115ce93ea2b1be85db5b4cc012
+
